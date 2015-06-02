167f6693b97b844999f10d4b8926b19de4a3f633
Helior Colorado <me@helior.info>
2015-06-01 18:59:50 -0700

    I go forward...

--- steps/navigation.js
+++ steps/navigation.js
@@ -1,4 +1,5 @@
 module.exports = function() {
-  this.Given(/^I visit "([^"]*)"$/, require('../lib/iVisit'));
-  this.Given(/^I go back$/,         require('../lib/iGoBack'));
+  this.Given(/^I visit "([^"]*)"$/,   require('../lib/iVisit'));
+  this.Given(/^I go back$/,           require('../lib/iGoBack'));
+  this.Given(/^I go forward$/,        require('../lib/iGoForward'));
 }
