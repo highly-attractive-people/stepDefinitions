769c9190107c734f64568f6dc9c3cee96b465f02
Helior Colorado <me@helior.info>
2015-06-01 19:00:03 -0700

    I refresh the page...

--- steps/navigation.js
+++ steps/navigation.js
@@ -2,4 +2,5 @@ module.exports = function() {
   this.Given(/^I visit "([^"]*)"$/,   require('../lib/iVisit'));
   this.Given(/^I go back$/,           require('../lib/iGoBack'));
   this.Given(/^I go forward$/,        require('../lib/iGoForward'));
+  this.Given(/^I refresh the page$/,  require('../lib/iRefresh'));
 }
