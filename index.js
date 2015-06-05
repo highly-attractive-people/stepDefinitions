02bd3aeadb7bc89f22b39a0340d774f0f3d9fd6c
Helior Colorado <me@helior.info>
2015-06-05 16:54:39 -0700

    Add String.prototype.startsWith & String.prototype.endsWith polyfills.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = function(options) {
+  // Polyfills
+  require('./lib/helpers/endWith.polyfill.js');
+  require('./lib/helpers/startWith.polyfill.js');
+
   require('./lib/world') .call(this, options);
   require('./steps/metadata').call(this);
   require('./steps/navigation').call(this);
