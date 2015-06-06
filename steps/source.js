51ff9f45a277103b7580ffe0d3f012c7756bcadd
Helior Colorado <me@helior.info>
2015-06-05 18:16:26 -0700

    Refactor htmlOfElementMatchesWith wrapper and step definition.
    Now supports the stringHelper features.

--- steps/source.js
+++ steps/source.js
@@ -1,5 +1,16 @@
+var compareTypes = require('../lib//helpers/stringHelperCompareTypeMapper');
+
 module.exports = function() {
-  this.Given(/^(?:the )?(inner )?html of "([^""]*)" element matches with:$/, require('../lib/htmlOfElementMatchesWith'));
+  this.Given(/^(?:the )?(inner )?html of the "([^""]*)" element (does not )?(strictly )?(match(?:es)?|contain(?:s)?|start(?:s)? with|end(?:s)? with) with:$/, function (isNotInclusive, selector, negate, strict, compareType, expected, callback) {
+    var includeSelectorTag = !!!isNotInclusive;
+    var options = {
+      caseSensitive: !!strict,
+      type: compareTypes[compareType],
+      negate: !!negate,
+      descriptor: 'html source'
+    };
+    require('../lib/htmlOfElementMatchesWith').call(this, callback, selector, includeSelectorTag, expected, options)
+  });
   // this.Given('the $attr attribute value of the $selector element matches $value');
   // this.Given('the $attr attribute value of the $selector element contains $value');
 }
