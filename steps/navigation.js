328cddf40ac22fc8e9ab7cf9f5d87f0b78fdd7eb
Helior Colorado <me@helior.info>
2015-06-05 19:11:55 -0700

    Sorting function signature of iVisit

--- steps/navigation.js
+++ steps/navigation.js
@@ -1,5 +1,7 @@
 module.exports = function() {
-  this.Given(/^I visit "([^"]*)"$/,   require('../lib/iVisit'));
+  this.Given(/^I visit "([^"]*)"$/, function (url, callback) {
+    require('../lib/iVisit').call(this, callback, url);
+  });
   this.Given(/^I go back$/,           require('../lib/iGoBack'));
   this.Given(/^I go forward$/,        require('../lib/iGoForward'));
   this.Given(/^I refresh the page$/,  require('../lib/iRefresh'));
