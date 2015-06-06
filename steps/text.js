62e7127ee593f5c017c05d27cb45dee484ec7ad4
Helior Colorado <me@helior.info>
2015-06-05 17:06:59 -0700

    Update text step definitions; remove cruft.

--- steps/text.js
+++ steps/text.js
@@ -1,4 +1,21 @@
 module.exports = function() {
-  this.Given(/^the "([^""]*)" element matches the text "([^""]*)"$/, require('../lib/elementMatchesTheText'));
-  this.Given(/^the "([^""]*)" element contains the text "([^""]*)"$/, require('../lib/elementContainsTheText'));
+  this.Given(/^the "([^""]*)" element (does not )?(strictly )?(match(?:es)?|contain(?:s)?|start(?:s)? with|end(?:s)? with) (?:the text )?"([^""]*)"$/, function(selector, negate, strict, compareType, expectedText, callback) {
+    var compareTypeMapper = {
+      'match': 'match',
+      'matches': 'match',
+      'contains': 'contain',
+      'contain': 'contain',
+      'starts with': 'startWith',
+      'ends with': 'endWith',
+      'start with': 'startWith',
+      'end with': 'endWith'
+    };
+
+    var options = {
+      caseSensitive: !!strict,
+      type: compareTypeMapper[compareType],
+      negate: !!negate
+    };
+    require('../lib/elementText').call(this, callback, selector, expectedText, options);
+  });
 }
