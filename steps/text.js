2669e180a27f3b96b57414e3b67ec74b8446e690
Helior Colorado <me@helior.info>
2015-06-05 18:14:21 -0700

    Move string Helper compareTypeMapper into its own file.

--- steps/text.js
+++ steps/text.js
@@ -1,19 +1,10 @@
+var compareTypes = require('../lib//helpers/stringHelperCompareTypeMapper');
+
 module.exports = function() {
   this.Given(/^the "([^""]*)" element (does not )?(strictly )?(match(?:es)?|contain(?:s)?|start(?:s)? with|end(?:s)? with) (?:the text )?"([^""]*)"$/, function(selector, negate, strict, compareType, expectedText, callback) {
-    var compareTypeMapper = {
-      'match': 'match',
-      'matches': 'match',
-      'contains': 'contain',
-      'contain': 'contain',
-      'starts with': 'startWith',
-      'ends with': 'endWith',
-      'start with': 'startWith',
-      'end with': 'endWith'
-    };
-
     var options = {
       caseSensitive: !!strict,
-      type: compareTypeMapper[compareType],
+      type: compareTypes[compareType],
       negate: !!negate
     };
     require('../lib/elementText').call(this, callback, selector, expectedText, options);
